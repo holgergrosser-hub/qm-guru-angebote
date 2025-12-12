0706463e230a3cec6fa07a127d390fe00c6fcf13
holgergrosser-hub <holger.grosser@iso9001.info>
2025-12-12 15:54:02 +0100

    Update SCRIPT_URL to new Google Apps Script URL

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 // DEINE SCRIPT URL
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz3czRfgd5UfSkHp2KGxYH_iqXCKIiY4k5d1zLPv-5z2KyEczDvH0DbHvok-x9mnjB9/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw3JA6YD0Kvr9iNi0s6rZTzZnOjJP6rN4tbyqaQPuRNRYiDjSdhixyeqIVV9FqFVSJa/exec';
 
 const styles = {
   container: {
